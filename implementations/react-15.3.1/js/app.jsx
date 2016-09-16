deb0d0e2483b6ab3f52104c4ac45a4f8679ea553
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-09-16 19:53:30 -0300

    Replace react uuid to use a simple incremental key, like elm does

--- implementations/react-15.3.1/js/app.jsx
+++ implementations/react-15.3.1/js/app.jsx
@@ -13,23 +13,6 @@ var app = app || {};
 	app.COMPLETED_TODOS = 'completed';
 
   app.Utils = {
-    uuid: function () {
-      /*jshint bitwise:false */
-      var i, random;
-      var uuid = '';
-
-      for (i = 0; i < 32; i++) {
-        random = Math.random() * 16 | 0;
-        if (i === 8 || i === 12 || i === 16 || i === 20) {
-          uuid += '-';
-        }
-        uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random))
-          .toString(16);
-      }
-
-      return uuid;
-    },
-
     pluralize: function (count, word) {
       return count === 1 ? word : word + 's';
     },
@@ -58,6 +41,7 @@ var app = app || {};
     this.key = key;
     this.todos = [];
     this.onChanges = [];
+    this.todoId = 0;
   };
 
   app.TodoModel.prototype.subscribe = function (onChange) {
@@ -70,7 +54,7 @@ var app = app || {};
 
   app.TodoModel.prototype.addTodo = function (title) {
     this.todos = this.todos.concat({
-      id: Utils.uuid(),
+      id: this.todoId++,
       title: title,
       completed: false
     });
